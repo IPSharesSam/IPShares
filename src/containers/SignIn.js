0eb1ad8c5f89f589247092e77f31d20f36016c49
mauricebuiten <maubui@gmail.com>
2017-11-23 10:54:56 +0100

    Add columns and images to signin page

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -9,6 +9,10 @@ import RaisedButton from 'material-ui/RaisedButton'
 import FlatButton from 'material-ui/FlatButton'
 import signIn from '../actions/user/sign-in'
 import './SignIn.css'
+import copyright from '../images/copyright.png'
+import information from '../images/information.png'
+import registered from '../images/registered.png'
+
 
 export class SignIn extends PureComponent {
   static propTypes = {
@@ -37,13 +41,13 @@ export class SignIn extends PureComponent {
 
   render() {
     return (
-      <GridList>
-        <GridTile className="landing-text" rows={4}>
+      <GridList className="gridlist-signin" cols={12}>
+        <GridTile className="gridtile-signin" cols={6} rows={2}>
           <h1>IP Shares, a decentral intellectual property economy</h1>
-          <p>Uniting creators, advisors, all IP facets and the market using it's own cryptotoken, the IP Share. From licensing your patent to auctioning your painting...</p> 
+          <p>Uniting creators, advisors, all IP facets and the market using it's own cryptotoken, the IP Share. From licensing your patent to auctioning your painting...</p>
           <p> Welcome to the next big thing.</p>
         </GridTile>
-        <GridTile rows={4}>
+        <GridTile className="gridtile-signin" cols={6} rows={2}>
           <Paper className="signin-paper">
             <h1>Sign in</h1>
             <form onSubmit={this.submitForm.bind(this)}>
@@ -59,7 +63,51 @@ export class SignIn extends PureComponent {
               label="Sign up" />
           </Paper>
         </GridTile>
+        <GridTile className="gridtile-signin" cols={4} rows={3}>
+          <img src={ copyright } alt="" />
+          <p>Design, Patents & Trademarks</p>
+          <p>
+            One-click import all of your
+            registered trademarks, patents
+            and designs.
+          </p>
+          <p>
+            Sell your actual product or
+            license your invention as you
+            like, directly from your IPS account
+          </p>
+        </GridTile>
+        <GridTile className="gridtile-signin" cols={4} rows={3}>
+        <img src={ information } alt="" />
+        <p>Copyright</p>
+        <p>
+        Timestamp all types of work,
+        creating inevitable, decentral
+        evidence of ownership.
+        </p>
+        <p>
+        License your creations like
+        songs, artwork, video's
+        and 3D designs.
+        </p>
+        
+        </GridTile>
+        <GridTile className="gridtile-signin" cols={4} rows={3}>
+        <img src={ registered } alt="" />
+        <p>Design, Patents & Trademarks</p>
+          <p>
+            One-click import all of your
+            registered trademarks, patents
+            and designs.
+          </p>
+          <p>
+            Sell your actual product or
+            license your invention as you
+            like, directly from your IPS account
+          </p>
+        </GridTile>
       </GridList>
+
     )
   }
 }
