98bc4a6e03ff5de7834110882950b97c9c52b320
mauricebuiten <maubui@gmail.com>
2017-11-27 17:42:26 +0100

    Add profile info

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,29 @@
-
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import {List, ListItem} from 'material-ui/List';
+
 
 
 class Profile extends PureComponent {
-
+    
+    checkStatus(bool) {
+        return bool.toString()
+    }
     render() {
+        const { currentUser } = this.props
         return (
-            <div >
-                <h2>Profile Settings</h2>
-                <p>
-                    Add Profile settings here
-                </p>
-            </div>
+            <List>
+                <ListItem>Username: { currentUser.firstName } { currentUser.lastName }</ListItem>
+                <ListItem>Adress: { currentUser.streetName } { currentUser.streetNumber }, { currentUser.postalCode }, { currentUser.country }</ListItem>
+                <ListItem>Email: { currentUser.email }</ListItem>
+                <ListItem>Phonenumber: { currentUser.phoneNumber }</ListItem>
+                <ListItem>Subcribed to newsletter: { this.checkStatus(currentUser.subscribed) }</ListItem>
+                <ListItem>Public profile: { this.checkStatus(currentUser.public) }</ListItem>
+                <ListItem>Tags: { currentUser.tags }</ListItem>
+            </List>
         )
     }
 }
 
-export default Profile
+const mapStateToProps = ({ currentUser }) => ({ currentUser })
+export default connect(mapStateToProps)(Profile)
