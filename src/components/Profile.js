5f57da2566f2388c92432462c71eba97ead04cf1
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-11-28 14:45:33 +0100

    added profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,26 +1,60 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import {List, ListItem} from 'material-ui/List';
+import { GridList, GridTile } from 'material-ui/GridList';
+import Toggle from 'material-ui/Toggle';
+import './Profile.css'
+import Paper from 'material-ui/Paper'
 
+const styles = {
+  block: {
+    maxWidth: 250,
+  },
+  toggle: {
+    marginBottom: 16,
+  },
+  thumbOff: {
+    backgroundColor: '#ffcccc',
+  },
+  trackOff: {
+    backgroundColor: '#ff9d9d',
+  },
+  thumbSwitched: {
+    backgroundColor: 'red',
+  },
+  trackSwitched: {
+    backgroundColor: '#ff9d9d',
+  },
+  labelStyle: {
+    color: 'red',
+  },
+};
 
 
 class Profile extends PureComponent {
-    
+
     checkStatus(bool) {
         return bool.toString()
     }
     render() {
         const { currentUser } = this.props
+
         return (
-            <List>
-                <ListItem>Username: { currentUser.firstName } { currentUser.lastName }</ListItem>
-                <ListItem>Adress: { currentUser.streetName } { currentUser.streetNumber }, { currentUser.postalCode }, { currentUser.country }</ListItem>
-                <ListItem>Email: { currentUser.email }</ListItem>
-                <ListItem>Phonenumber: { currentUser.phoneNumber }</ListItem>
-                <ListItem>Subcribed to newsletter: { this.checkStatus(currentUser.subscribed) }</ListItem>
-                <ListItem>Public profile: { this.checkStatus(currentUser.public) }</ListItem>
-                <ListItem>Tags: { currentUser.tags }</ListItem>
-            </List>
+
+
+          <div className="profile-element">
+            <Paper className = "profile-paper">
+
+              <p>  Username: <text className="inside-item">{ currentUser.firstName } { currentUser.lastName }</text></p>
+              <p>  Adress: <text className="inside-item">{ currentUser.streetName } { currentUser.streetNumber }, { currentUser.postalCode }, { currentUser.country }</text></p>
+              <p>  Email: <text className="inside-item">{ currentUser.email }</text></p>
+              <p>  Phonenumber: <text className="inside-item">{ currentUser.phoneNumber }</text></p>
+              <p>  <Toggle className = "profile-toggle" labelPosition="left" label = "Subcribed to newsletter:" defaultToggled={ this.checkStatus(currentUser.subscribed) } style={styles.toggle} /></p>
+              <p>  <Toggle className = "profile-toggle" labelPosition="left" label = "Public profile:" defaultToggled ={ this.checkStatus(currentUser.public) } style={styles.toggle} /></p>
+              <p>  Tags: <text className="inside-item">{ currentUser.tags }</text></p>
+            </Paper>
+
+          </div>
         )
     }
 }
