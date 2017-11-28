2b05c9ad5b37dbfaae290aec8de812eeec184ebf
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-11-28 16:36:31 +0100

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,21 @@
 import React, { PureComponent } from 'react'
-
+import './Footer.css'
+import { logo} from '../images'
 const styles = {
     backgroundColor: '#595959',
     height: '256px',
     position: 'relative',
+
+
 }
 class Footer extends PureComponent {
-    
+
   render() {
     return (
       <div style={ styles } className="Footer" >
+        <h3 className="footer-text">Founded with love from Amsterdam</h3>
+        <img className="footer-logo" src={ logo } alt="" />
+        
       </div>
     )
   }
