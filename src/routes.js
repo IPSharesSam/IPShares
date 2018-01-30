cfeeeb084b98d794fc2b090829e2c5860d65fe2b
opaismoe <wiebe.buursink@gmail.com>
2018-01-30 11:31:30 +0100

    Basic wireframe PublicProfile

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import Home from './containers/Home'
+import PublicProfile from './containers/PublicProfile'
 // import SignUp from './users/SignUp'
 // import SignIn from './users/SignIn'
 
@@ -9,6 +10,7 @@ export default class Routes extends Component {
     return (
       <div>
         <Route exact path="/" component={Home} />
+        <Route exact path="/Profiles" component={PublicProfile} />
         {/* <Route path="/sign-up" component={SignUp} />
         <Route exact path="/" component={SignIn} /> */}
       </div>
