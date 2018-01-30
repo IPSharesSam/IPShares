89887147c3089b52f2f757729e789715508639ba
ELEXILON <elexilon@gmail.com>
2018-01-30 11:33:09 +0100

    create signin

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import Home from './containers/Home'
 // import SignUp from './users/SignUp'
-// import SignIn from './users/SignIn'
+import SignIn from './containers/SignIn'
 
 export default class Routes extends Component {
   render() {
     return (
       <div>
         <Route exact path="/" component={Home} />
-        {/* <Route path="/sign-up" component={SignUp} />
-        <Route exact path="/" component={SignIn} /> */}
+        <Route path="/sign-in" component={SignIn} />
       </div>
     )
   }
