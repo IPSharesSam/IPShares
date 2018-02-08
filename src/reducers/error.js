d9d79372aa7fe057179fc03f3f8b11d7ffc1aa0e
Iskander Broere <iskanderbroere@gmail.com>
2018-02-08 14:29:45 +0100

    fix error

--- src/reducers/error.js
+++ src/reducers/error.js
@@ -1,13 +1,17 @@
 import { LOAD_ERROR, CLEAR_ERROR } from '../actions/user/loading'
+import { instanceOf } from '../../../../../.cache/typescript/2.6/node_modules/@types/prop-types';
 
-export default function(state = null, { type, payload } = {}) {
+export default function (state = null, { type, payload } = {}) {
   switch (type) {
     case LOAD_ERROR:
-    console.log(payload);
-    if (payload instanceof Error) {
-      return payload.response.body.message
-    }
-    return payload
+      console.log(payload);
+      if (payload instanceof Error) {
+        return payload.response.body.message
+      }
+      if (payload instanceof String) {
+        return payload
+      }
+      return null
 
     case CLEAR_ERROR:
       return null
